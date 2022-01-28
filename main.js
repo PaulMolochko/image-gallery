2538e9a4484292077a9b3f042d7d6f6830ed453b
PaulMolochko <paulmolochko@outlook.com>
2022-01-28 16:47:59 +0200

    changed the path in the imgArray and adjusted the loops

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames.
 Javascript does not have access to the filename system, so I have to hard-code this in.
 */
-const imgArray = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg', 'pic5.jpg'];
+const imgArray = ['images/pic1.jpg','images/pic2.jpg','images/pic3.jpg','images/pic4.jpg', 'images/pic5.jpg'];
 /* Looping through images 
 Several way to do it.*/
 //for(;;) loop
 /*
 for (let i = 0; i < imgArray.length; i++){
 const newImage = document.createElement('img');
-newImage.setAttribute('src', 'images/pic${i}'); //could also declare the path itself into the imgArray ('./pic1.jpg'), and do newImage.setAttribute('src',imgArray[i]);
+newImage.setAttribute('src', imgArray[i]);
 thumbBar.appendChild(newImage);
 }
 */
@@ -22,14 +22,14 @@ thumbBar.appendChild(newImage);
 /*
 for (const image of imgArray) {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/${image}'); //could also declare the path itself into the imgArray ('./pic1.jpg'), and do newImage.setAttribute('src',image);
+    newImage.setAttribute('src', image);
     thumbBar.appendChild(newImage);
 }
 */
 // forEach array method
 imgArray.forEach(image => {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/${image}'); //could also declare the path itself into the imgArray ('./pic1.jpg'), and do newImage.setAttribute('src',image);
+    newImage.setAttribute('src', image);
     thumbBar.appendChild(newImage); 
 })
 
