bed0c56083f486e530b56839d2eb4445d9137e5b
PaulMolochko <paulmolochko@outlook.com>
2022-01-28 16:27:34 +0200

    declared the filename array

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
-
+/* Declaring the array of image filenames.
+Javascript does not have access to the filename system, so I have to hard-code this in.
+*/
+const imgArray = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg', 'pic5.jpg'];
 /* Looping through images */
 
 const newImage = document.createElement('img');
