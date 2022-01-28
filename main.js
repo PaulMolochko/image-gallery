3ffa669979d8026efa4752c4da952c336aff774c
PaulMolochko <paulmolochko@outlook.com>
2022-01-28 16:38:53 +0200

    added multiple ways to iterate through the array

--- main.js
+++ main.js
@@ -8,10 +8,29 @@ const overlay = document.querySelector('.overlay');
 Javascript does not have access to the filename system, so I have to hard-code this in.
 */
 const imgArray = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg', 'pic5.jpg'];
-/* Looping through images */
-
+/* Looping through images 
+Several way to do it.*/
+//for(;;) loop
+/*
+for (let i = 0; i < imgArray.length; i++){
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
+newImage.setAttribute('src', 'images/pic${i}'); //could also declare the path itself into the imgArray ('./pic1.jpg'), and do newImage.setAttribute('src',imgArray[i]);
 thumbBar.appendChild(newImage);
+}
+*/
+//for-of from ES5
+/*
+for (const image of imgArray) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/${image}'); //could also declare the path itself into the imgArray ('./pic1.jpg'), and do newImage.setAttribute('src',image);
+    thumbBar.appendChild(newImage);
+}
+*/
+// forEach array method
+imgArray.forEach(image => {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/${image}'); //could also declare the path itself into the imgArray ('./pic1.jpg'), and do newImage.setAttribute('src',image);
+    thumbBar.appendChild(newImage); 
+})
 
 /* Wiring up the Darken/Lighten button */
